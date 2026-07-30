707819d762dc069a9f9a926c200d82eae230ffde
NIRANJAN CREATIONS <hackerenvironment1514@gmail.com>
2026-07-30 19:13:54 +0530

    Update k-windows.py

--- k-windows.py
+++ k-windows.py
@@ -1,68 +1,116 @@
-from os import system
+from os import system, path
+from sys import argv, exit
 
-from sys import argv,exit
 
-if len(argv) == 1 :
-	print("Error : Invalide Arguments ...!")
-	print("Usage : k <arg>")
-	exit()
+K_DRIVE = r"C:\K_DRIVE"
+
+
+if len(argv) == 1:
+    print("Error : Invalid Arguments ...!")
+    print("Usage : k <arg>")
+    exit()
+
 
 def start():
-	system(r"mkdir C:\K_DRIVE")
 
-	uname = input("Enter Git-Hub User Name : ")
+    if path.exists(K_DRIVE):
+        print("K Drive already exists ...!")
+        return
 
-	repo  = input("Enter Git-Hub Repo Name : ")
-	
-	system(f"git clone https://github.com/{uname}/{repo}.git C:\\K_DRIVE")
+    system(f'mkdir "{K_DRIVE}"')
 
-	system(r"subst K: C:\K_DRIVE")
+    uname = input("Enter Git-Hub User Name : ")
+
+    repo = input("Enter Git-Hub Repo Name : ")
+
+    system(
+        f'git clone https://github.com/{uname}/{repo}.git "{K_DRIVE}"'
+    )
+
+    system(
+        f'subst K: "{K_DRIVE}"'
+    )
+
+    print("K Drive Started Successfully !")
+
 
 def activate():
-	system("start cmd /k cd /d K:\\")
+
+    system("start cmd /k cd /d K:\\")
+
 
 def update():
-	system(r'git -C  "C:\K_DRIVE" add -A ')
-	msg = input('Enter Your Update Message : ")
-	system(rf'git -C "C:\K_DRIVE" commit -m "{msg}"')
-	system(r'git -C "C:\K_DRIVE" push ')
+
+    system(f'git -C "{K_DRIVE}" add -A')
+
+    msg = input("Enter Your Update Message : ")
+
+    system(
+        f'git -C "{K_DRIVE}" commit -m "{msg}"'
+    )
+
+    system(
+        f'git -C "{K_DRIVE}" push'
+    )
+
+    print("Updated Successfully !")
+
 
 def end():
-	system("subst K: /D")
-	system(r"rmdir /S /Q C:\K_DRIVE")
+
+    system("subst K: /D")
+
+    system(
+        f'rmdir /S /Q "{K_DRIVE}"'
+    )
+
+    print("K Drive Removed Successfully !")
+
 
 def help():
-	print("+===========================================================+")
-	print("|             K - Drive ( Git - Hub Cloud )                 |")
-	print("+===========================================================+")
-	print("| Developer : Niranjan Kuamr K                              |")
-	print("+===========================================================+")
-	print("|Usage      : k <arg>                                       |")
-	print("+===========================================================+")
-	print()
-	print(" arg             use")
-	print(" ---             ---")
-	print(" start           to starting k drive with specific username and repo")
-	print(" activate        to activating K - Drive")
-	print(" update          to updating all changes to repo")
-	print(" end             to exiting from K drive and removing connection to repo ")
-	print(" help            to knowing what it is and how to use ")
-	print()
-	print(" Note        :    Public Repo is recommended for avoiding authentication")
-	print(" Required    :    Git install")
-	print(" git install :    https://git-scm.com/install")
+
+    print("+===========================================================+")
+    print("|             K - Drive ( Git - Hub Cloud )                 |")
+    print("+===========================================================+")
+    print("| Developer : Niranjan Kumar K                              |")
+    print("+===========================================================+")
+    print("| Usage     : k <arg>                                       |")
+    print("+===========================================================+")
+
+    print()
+
+    print(" arg             use")
+    print(" ---             ---")
+    print(" start           start K Drive with GitHub repository")
+    print(" activate        activate K Drive")
+    print(" update          push all changes to GitHub")
+    print(" end             remove K Drive")
+    print(" help            show help")
+
+    print()
+
+    print(" Note        : Public repository recommended")
+    print(" Required    : Git installation")
+    print(" Git install : https://git-scm.com/install")
+
 
 arg = argv[1].lower()
 
+
 if arg == "start":
-	start()
+    start()
+
 elif arg == "activate":
-	activate();
-elif arg == "end" :
-	end()
+    activate()
+
 elif arg == "update":
-	update()
+    update()
+
+elif arg == "end":
+    end()
+
 elif arg == "help":
-	help()
-else :
-	print("Error : Invalide Argument ...!")
+    help()
+
+else:
+    print("Error : Invalid Argument ...!")
